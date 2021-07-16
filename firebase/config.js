e8cf1bca622e96762193b736d01780bf5e6a7d41
Jose Domenico <domenicodiaz.joseangel@gmail.com>
2021-07-16 14:36:37 -0600

    App terminada

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0qPiSlyx8DbYrHI21IQlY8kPr_NzOwAk",
+    authDomain: "product-hunt-fdadf.firebaseapp.com",
+    projectId: "product-hunt-fdadf",
+    storageBucket: "product-hunt-fdadf.appspot.com",
+    messagingSenderId: "219963531621",
+    appId: "1:219963531621:web:667a46abaa8e352e1ec18c"
+};
+
+export default firebaseConfig
